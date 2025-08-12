ac6683b4d8a61b1c034e9c49c0158d23691e26be
tah315 <clintontah2006@gmail.com>
2025-08-12 14:20:04 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'DOLLA', // Your name
+  botOwner: 'Tāh Çlīñtōñ🤑', // Your name
   ownerNumber: '237652050160', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
